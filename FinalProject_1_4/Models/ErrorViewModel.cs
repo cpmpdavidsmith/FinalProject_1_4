b415ae4748bbb0df13c3d195d0d75f1719e81a9d
David Smith <cpmpdavidsmith@icloud.com>
2023-03-20 11:40:10 -0400

    gitig

---
+++ FinalProject_1_4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalProject_1_4.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
